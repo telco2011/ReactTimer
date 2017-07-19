5eca2b1008880d66b4b8f960bbce3417851daa34
kratos <davidlopez.david@gmail.com>
2017-07-19 17:32:25 +1000

    Update countdown

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -25,6 +25,11 @@ var Countdown = React.createClass({
       }
     }
   },
+  componentWillUnmount: function () {
+    console.log('componentWillunMount');
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
   startTimer: function () {
     this.timer = setInterval(() => {
       var newCount = this.state.count - 1;
@@ -33,6 +38,9 @@ var Countdown = React.createClass({
         count: newCount >= 0 ? newCount : 0
       });
 
+      if (newCount === 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
     }, 1000);
   },
   handleSetCountdown: function (seconds) {
